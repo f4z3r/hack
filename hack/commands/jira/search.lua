874e6fa2611d24786ce5d7e9a7c28172a0e5b03b
Jakob Beckmann <f4z3r-github@pm.me>
2026-04-02 08:04:53 +0200

    feat(jira): allow using assignee for ticket search
    Signed-off-by: Jakob Beckmann <f4z3r-github@pm.me>

--- hack/commands/jira/search.lua
+++ hack/commands/jira/search.lua
@@ -12,8 +12,9 @@ M.COMMAND_ARRAY = { "jira", "search" }
 function M.register_command(p)
   local parser = p:command("search s"):summary("Search issues"):description("Search issues based on a provided term.")
 
-  parser:argument("term", "The term to search for.")
-  parser:argument("project", "The project(s) to search."):args("+")
+  parser:argument("term", "The term to search for."):args("?")
+  parser:option("-p --project", "The project to search."):count("+")
+  parser:option("-a --assignee", "The assignee to filter by.")
   parser:flag("-c --closed", "Include closed issues in the search.")
   parser:flag("-i --ids", "Just output the list of IDs.")
   parser:flag("-d --description", "Also search within the description.")
@@ -26,13 +27,19 @@ function M.execute(server, options)
   local query_parts = {
     string.format("project in (%s)", table.concat(options.project, ", ")),
   }
-  local term_query = string.format('summary ~ "%s"', options.term)
-  if options.description then
-    term_query = string.format('(%s OR description ~ "%s")', term_query, options.term)
+  if options.term then
+    local term_query = string.format('summary ~ "%s"', options.term)
+    if options.description then
+      query_parts[#query_parts + 1] = string.format('(summary OR description ~ "%s")', term_query, options.term)
+    else
+      query_parts[#query_parts + 1] = term_query
+    end
   end
-  query_parts[#query_parts + 1] = term_query
   if not options.closed then
-    query_parts[#query_parts + 1] = "status not in (Cancelled, Done)"
+    query_parts[#query_parts + 1] = "status not in (Cancelled, Done, Closed)"
+  end
+  if options.assignee then
+    query_parts[#query_parts + 1] = string.format("assignee = '%s'", options.assignee)
   end
   local jql = table.concat(query_parts, " AND ") .. " ORDER BY priority DESC, updated DESC"
   log:info("final jql query", "jql", jql)
